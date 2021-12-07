b3742e4e03692327ea16da1546e680c3643d92f3
murari03 <72371521+murari03@users.noreply.github.com>
2021-12-07 19:42:01 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PT7PC4');</script>
+<!-- End Google Tag Manager -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <title>VIK | Visual Interaktif Kompas</title>
+    <meta name="theme-color" content="#000">
+    <meta name="msapplication-navbutton-color" content="#000">
+    <meta property="og:type" content="article" />
+    <!-- s: fb meta -->
+    <meta property="og:url" content="" />
+    <meta property="og:title" content="VIK | Visual Interaktif Kompas" />
+    <meta property="og:description" content="Upaya jurnalistik harian Kompas, Kompas.com, dan KompasTV (multi-channel) mencari kedalaman, arti, dan perspektif atas suatu peristiwa, disajikan secara naratif yang diperkaya grafis, foto, video, maupun audio (multi-media) untuk memberi warna baru di dunia jurnalistik." />
+    <meta property="og:image" content="https://asset.kompas.com/data/2016/vik/images/thumb.jpg" />
+    <meta property="fb:admins" content="1386097730" />
+    <meta property="fb:app_id" content="161133340609176" />
+    <!-- S:tweeter card -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@kompascom" />
+    <meta name="twitter:creator" content="@kompascom">
+    <meta name="twitter:title" content="VIK %7C Visual Interaktif Kompas" />
+    <meta name="twitter:description" content="Upaya jurnalistik harian Kompas, Kompas.com, dan KompasTV (multi-channel) mencari kedalaman, arti, dan perspektif atas suatu peristiwa, disajikan secara naratif yang diperkaya grafis, foto, video, maupun audio (multi-media) untuk memberi warna baru di dunia jurnalistik." />
+    <meta name="twitter:image" content="https://asset.kompas.com/data/2016/vik/images/thumb.jpg" />
+    <!-- E:tweeter card -->
+    <link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
+    <link type="text/css" rel="stylesheet" href="https://asset.kompas.com/data/2016/vik/icomoon/style.css">
+    <link rel="shortcut icon" href="https://asset.kompas.com/data/2016/vik/images/favicon.png">
+    <link type="text/css" rel="stylesheet" href="https://asset.kompas.com/data/2016/vik/css/style.css">
+    <script src="https://asset.kompas.com/data/2016/vik/js/scrollreveal.min.js"></script>
+    <script src="https://asset.kompas.com/data/2014/kompascom/js/jquery.js"></script>
+	
+	<meta name="adx:sections" content="VIK" />		
+<!-- ProPS Insight script -->
+<script type="text/javascript">
+ var _siteId="10003299287";
+ (function(){
+   var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="//st-a.props.id/insight.js";
+   var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
+</script>
+<!-- End of ProPS Insight script -->
+
+</head>
+
+<body>
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PT7PC4" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<div class="logo-top">
+			<h1 class="clearfix">
+				<a href="" class="logo-title">
+				<span class="title-text">VIK</span>
+				<span class="block-logo">
+					<span class="title-break">Visual</span>
+					<span class="title-break">Interaktif</span>
+					<span class="title-break">Kompas</span>
+					
+				</span>
+				</a>
+			</h1>
+		</div>
+		<div class="at-all">
+			<a href="https://vik.kompas.com/index.php/all" class="visit-link-all">
+				<span class="icon-list icon-class"></span>
+				<span class="in-left">
+					<span>Lihat</span>
+					<span>Semua</span>
+				</span>
+			</a>
+		</div>
+		<div class="wrap-menu">
+			<div class="table-content">
+				<ul class="menu-list">
+					<li class="menu-item">
+						<a href="" class="menu-item-link">Home</a>
+					</li>
+					<li class="menu-item">
+						<a href="" class="menu-item-link">Home2</a>
+					</li>
+					<li class="menu-item">
+						<a href="" class="menu-item-link">Hom3</a>
+					</li>
+					<li class="menu-item">
+						<a href="" class="menu-item-link">Home4</a>
+					</li>
+					<li class="menu-item">
+						<a href="" class="menu-item-link">Home5</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="still-menu still-menu-btm">
+			<div class="arrows">
+				<div class="in-link anchor-up">
+					<span class="icon-up"></span>
+				</div>
+				<div class="in-link anchor-down">
+					<span class="icon-down"></span>
+				</div>	
+			</div>	
+		</div>
+
+
+
+
+<div id="fullpage">
+
+	<div class="section" data-anchor="205">
+    	
+		<div class="the-overlay"></div>
+		<div class="overlay-wrap">
+			<div class="container">
+				<div class="box-in-lead">
+									<div class="top-title">
+						Harapan Jurnalisme Berkelanjutan					</div>
+
+					<div class="lead-title">
+						Berjuang untuk independen dan sustainable					</div>
+					<div class="text-preview">
+						
+					</div>
+					<a target="_blank" href="https://vik.kompas.com/harapan-jurnalisme-berkelanjutan/" class="visit-link"><span>LIHAT</span></a>
+				</div>
+			</div>
+		</div>
+
+		<div class="bg-slide">
+			<div class="on-background" style="background: url('https://asset.kompas.com/data/photo/2021/09/14/61400caa5e7e8.jpg') center center no-repeat;"></div>
+		</div>
+</div>
+
+<!-- FullPage -->
+<script type="text/javascript" src="https://asset.kompas.com/data/2016/vik/js/fullPage/jquery.fullPage.min.js"></script>
+<script type="text/javascript" src="https://asset.kompas.com/data/2016/vik/js/fullPage/jquery.easings.min.js"></script>
+<script type="text/javascript" src="https://asset.kompas.com/data/2016/vik/js/fullPage/jquery.slimscroll.min.js"></script>
+
+<script type="text/javascript" src="https://asset.kompas.com/data/2016/vik/js/jquery.mousewheel.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/scrollreveal.js/3.0.9/scrollreveal.min.js"></script>
+
+<!-- Slick -->
+<script src="https://asset.kompas.com/data/2016/vik/js/slick/slick.min.js"></script>
+
+<script type="text/javascript">
+
+
+	$(document).ready(function() {
+
+		window.sr = ScrollReveal({ reset: true,vFactor: 0.20  });
+
+		var fullIndex = $('.section').length;
+
+	    $('#fullpage').fullpage({
+	    	anchors:[
+						'205',
+						'204',
+						'203',
+						'202',
+						'201',
+						'200',
+						'199',
+						'198',
+						'197',
+						'196',
+		    	],
+			scrollingSpeed: 800,
+			fitToSectionDelay: 100,
+			easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.1)',
+			afterLoad: function(direction,index){
+	            if (index == fullIndex) {
+	            	$('.anchor-down').css('opacity','0');
+	            } else {
+	            	$('.anchor-down').css('opacity','1');
+	            }
+
+	            if (index == 1) {
+	            	$('.anchor-up').css('opacity','0');
+	            } else {
+	            	$('.anchor-up').css('opacity','1');
+	            }
+	        }
+
+	    });
+
+        $('.vertical-list').mousewheel(function(event) {
+            // console.log(event.deltaX, event.deltaY, event.deltaFactor);
+            ar = $(this).hasClass('open');
+            if (event.deltaY > 0 ) {
+                if (ar == false) {
+                    slickon.slick("slickPrev");
+                    $(this).addClass('open');
+                }
+            } else {
+                if (ar == false) {
+                   slickon.slick("slickNext");
+                    $(this).addClass('open');
+                }
+            }
+        });
+
+		$('.block-click').click(function(event) {
+			$(this).toggleClass('open');
+			$('.wrap-menu').toggleClass('open');
+		});
+
+		$('.anchor-up').click(function(event) {
+			$.fn.fullpage.moveSectionUp();
+		});
+		$('.anchor-down').click(function(event) {
+			$.fn.fullpage.moveSectionDown();
+		});
+	});
+</script>
+</html>
